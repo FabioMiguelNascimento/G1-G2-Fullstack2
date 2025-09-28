465538c2571ba9671706904f896381a2b5c610c2
vitorotto <vitorotto05@gmail.com>
2025-09-28 12:58:11 -0300

    frontend: feat: sidebar da conta ajustada para pegar os dados do usuário logado

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Star, User } from "lucide-react";
+import { Star, User as UserIcon } from "lucide-react";
 import * as React from "react";
 
 import { NavMain } from "@/components/nav-main";
@@ -14,38 +14,54 @@ import {
     SidebarTrigger,
 } from "@/components/ui/sidebar";
 
-// This is sample data.
-const data = {
-    user: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-        avatar: "/avatars/shadcn.jpg",
-    },
-    navMain: [
-        {
-            title: "Meus dados",
-            url: "/account",
-            icon: User,
-        },
-        {
-            title: "Favoritos",
-            url: "/account/favorites",
-            icon: Star,
-        },
-    ],
+import useAuthContext from "@/hooks/useAthContext";
+import type { User } from "@/types/user.type";
+
+const buildUserData = (user: User | null) => {
+    if (user) {
+        return {
+            name: user.name,
+            email: user.email,
+            avatar: `/avatars/${user.name.charAt(0).toLowerCase()}.jpg`, // Avatar baseado na inicial
+        };
+    }
+    return {
+        name: "Usuário",
+        email: "usuario@example.com",
+        avatar: "/avatars/default.jpg",
+    };
 };
 
+const buildNavMain = (user: User | null) => [
+    {
+        title: "Minha conta",
+        url: user ? `/account/${user.id}` : "/account/",
+        icon: UserIcon,
+    },
+    {
+        title: "Favoritos",
+        url: "/account/favorites",
+        icon: Star,
+    },
+];
+
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    const { user } = useAuthContext();
+    
+    // Estruturando os dados dinamicamente baseado no usuário logado
+    const userData = buildUserData(user);
+    const navMainItems = buildNavMain(user);
+    
     return (
         <Sidebar collapsible="icon" {...props} className="pt-20">
             <SidebarHeader>
                 <SidebarTrigger className="-ml-1" />
             </SidebarHeader>
             <SidebarContent>
-                <NavMain items={data.navMain} />
+                <NavMain items={navMainItems} />
             </SidebarContent>
             <SidebarFooter>
-                <NavUser user={data.user} />
+                <NavUser user={userData} />
             </SidebarFooter>
             <SidebarRail />
         </Sidebar>
